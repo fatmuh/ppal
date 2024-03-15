b4dca795535a744032d9b3417411bf8c5226468e
Fathur Muhammad <fatmuhdev@gmail.com>
2024-03-16 04:06:59 +0700

    add favicon form

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -9,6 +9,10 @@
     <title>KTA - PPAL</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.4/axios.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link
+        href="{{ asset('assets/img/logo.png') }}"
+        rel="shortcut icon"
+    >
 </head>
 
 <body class="bg-gray-100">
