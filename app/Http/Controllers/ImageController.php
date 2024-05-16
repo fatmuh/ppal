cf670aaaa5b2a1001ae0c7228059e9dd181ef725
Fathur Muhammad <fatmuhdev@gmail.com>
2024-05-16 21:42:46 +0700

    Image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,41 +3,20 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    public function getImage(Request $request)
+    public function getImage()
     {
-        $imageUrl = 'https://database.ppal.or.id/ppal/' . $request->input('image');
-
-        try {
-            $client = new \GuzzleHttp\Client();
-            $response = $client->get($imageUrl);
-
-            if ($response->getStatusCode() == 200) {
-                $contentType = $response->getHeader('Content-Type');
-                if (!empty($contentType)) {
-                    return response($response->getBody())->header('Content-Type', $contentType[0]);
-                } else {
-                    // Log the headers received for debugging purposes
-                    Log::error("Content-Type header not found for URL: " . $imageUrl);
-                    Log::error("Response headers: " . json_encode($response->getHeaders()));
-                    // Use a default Content-Type as fallback
-                    return response($response->getBody())->header('Content-Type', 'image/jpeg');
-                }
-            } else {
-                Log::error("Failed to retrieve image. Status code: " . $response->getStatusCode());
-                return response("Error retrieving image", 500);
-            }
-        } catch (RequestException $e) {
-            Log::error("RequestException: " . $e->getMessage());
-            return response("Error retrieving image", 500);
-        } catch (\Exception $e) {
-            Log::error("Exception: " . $e->getMessage());
-            return response("Error retrieving image", 500);
+        if (Storage::disk('ftp')->exists("foto/MxmEsJHod9nH4KGN8azuZBv6cn8Et2lWLAYE3sNW.jpg")) {
+            $fileContent = Storage::disk('ftp')->get("foto/MxmEsJHod9nH4KGN8azuZBv6cn8Et2lWLAYE3sNW.jpg"); // Mengambil file dari FTP
+    
+            return response($fileContent)
+                ->header('Content-Type', 'image/jpeg'); // Sesuaikan dengan tipe file
+        } else {
+            abort(404, 'File not found');
         }
     }
 }
