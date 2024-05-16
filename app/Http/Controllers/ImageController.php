bec4784d3eaba51913df23ea787121b2e2a56ce0
Fathur Muhammad <fatmuhdev@gmail.com>
2024-05-16 21:23:55 +0700

    Image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,20 +3,38 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ImageController extends Controller
 {
     public function getImage(Request $request)
     {
-        // URL gambar dari domain eksternal
         $imageUrl = 'https://database.ppal.or.id/ppal/' . $request->input('image');
 
-        // Menggunakan GuzzleHttp untuk mengambil gambar
-        $client = new Client();
-        $response = $client->get($imageUrl);
+        try {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->get($imageUrl);
 
-        // Mengembalikan gambar kepada klien
-        return response($response->getBody())->header('Content-Type', 'image/jpeg');
+            if ($response->getStatusCode() == 200) {
+                $contentType = $response->getHeader('Content-Type');
+                if (!empty($contentType)) {
+                    return response($response->getBody())->header('Content-Type', $contentType[0]);
+                } else {
+                    Log::error("Content-Type header not found for URL: " . $imageUrl);
+                    return response("Content-Type header not found", 500);
+                }
+            } else {
+                Log::error("Failed to retrieve image. Status code: " . $response->getStatusCode());
+                return response("Error retrieving image", 500);
+            }
+        } catch (RequestException $e) {
+            Log::error("RequestException: " . $e->getMessage());
+            return response("Error retrieving image", 500);
+        } catch (\Exception $e) {
+            Log::error("Exception: " . $e->getMessage());
+            return response("Error retrieving image", 500);
+        }
     }
 }
