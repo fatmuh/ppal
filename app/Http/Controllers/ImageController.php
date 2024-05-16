b2eda7b7f7763e4a6401c54967b258382287b686
Fathur Muhammad <fatmuhdev@gmail.com>
2024-05-16 21:16:42 +0700

    Image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,29 +4,19 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class ImageController extends Controller
 {
     public function getImage(Request $request)
     {
-        $url = 'https://database.ppal.or.id/ppal/foto/MxmEsJHod9nH4KGN8azuZBv6cn8Et2lWLAYE3sNW.jpg';
+        // URL gambar dari domain eksternal
+        $imageUrl = 'https://database.ppal.or.id/ppal/' . $request->input('image');
+
+        // Menggunakan GuzzleHttp untuk mengambil gambar
         $client = new Client();
-        
-        // Unduh gambar dari URL
-        $response = $client->get($url);
+        $response = $client->get($imageUrl);
 
-        if ($response->getStatusCode() == 200) {
-            // Ambil konten gambar
-            $imageContent = $response->getBody()->getContents();
-
-            // Dapatkan tipe konten gambar jika tersedia
-            $contentType = $response->hasHeader('Content-Type') ? $response->getHeader('Content-Type')[0] : 'application/octet-stream';
-
-            // Kembalikan gambar langsung dalam respons dengan header yang sesuai
-            return response($imageContent, 200)->header('Content-Type', $contentType);
-        } else {
-            return response()->json(['message' => 'Failed to download image'], 500);
-        }
+        // Mengembalikan gambar kepada klien
+        return response($response->getBody())->header('Content-Type', 'image/jpeg');
     }
 }
